e16a0247ff1414a1b85c00f90f93508deec1614d
Carlos Montes <krlosbergara@gmail.com>
2025-02-12 10:51:51 -0500

    agregar lógica para deshabilitar el botón de envío y mostrar mensajes de éxito/error en la recuperación de contraseña

--- src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { toast } from 'ngx-sonner';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
@@ -10,25 +11,52 @@ import { ButtonComponent } from 'src/app/shared/components/button/button.compone
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
   standalone: true,
-  imports: [FormsModule, RouterLink, ButtonComponent, CommonModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    RouterLink,
+    ButtonComponent,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   providers: [AuthService],
 })
 export class ForgotPasswordComponent implements OnInit {
   nit: string = '';
-  constructor(private readonly authService:AuthService) {}
+  disabledSendButton: boolean = false;
+  constructor(private readonly authService: AuthService,private readonly router:Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
   onSubmit() {
+
     this.authService.sendEmailRecovery(this.nit).subscribe(
       (response: any) => {
+        this.disabledSendButton = true
         console.log(response);
+        const msg = 'Operación exitosa';
+        toast.success(msg, {
+          position: 'bottom-right',
+          description:
+            'Si el documento es válido, recibirá un correo de recuperación.',
+        });
+        setTimeout(() => {
+          this.router.navigate(['/auth/sign-in']);
+        }, 2000);
       },
       (error: any) => {
         console.error(error);
-      }
-    )
+        const msg = 'La operación no se pudo completar';
+        toast.error(msg, {
+          position: 'bottom-right',
+          description: 'Intente mas tarde.',
+          action: {
+            label: 'Salir',
+            onClick: () => this.router.navigate(['/auth/sign-in']),
+          },
+          actionButtonStyle: 'background-color:#DC2626; color:white;',
+        });
+      },
+    );
   }
-
-
-
 }
